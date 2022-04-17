e585a71512a9fd565d2f7a785eb5f9819fa7df28
Stephen Ip <91226924+stephen-ip@users.noreply.github.com>
2022-04-17 07:18:12 -0700

    leaderboard nav + other users page stats

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -1,14 +1,16 @@
 import React from "react";
 
-function username({ user }) {
+function username({ user, bestscores }) {
   return (
     <div>
       {user ? (
         <div>
+          <img className="userpfp" src={user.pfpurl}/>
           <p>user firstname: {user.firstname}</p>
           <p>user lastname: {user.lastname}</p>
           <p>user username: {user.username}</p>
           <p>user email: {user.email}</p>
+          <p>best scores: {JSON.stringify(bestscores)}</p>
         </div>
       ) : (
         "no user with this username"
@@ -27,9 +29,45 @@ export async function getServerSideProps(context) {
       return datajson;
     }
   );
+  const games = ["memorytiles", "numbermemo", "cardflip", "slidepuzzle"];
+  const bestScores = {
+    memorytiles: null,
+    numbermemo: null,
+    cardflip: null,
+    slidepuzzle: null,
+  };
+  if (data.user) {
+    for (var i = 0; i < games.length; i++) {
+      const game = games[i];
+      await fetch(`http://localhost:3000/api/stats/scores/desc/${game}`, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          for (var i = 0; i < json.length; i++) {
+            const record = json[i];
+            if (record.username == data.user.username) {
+              if (game != "slidepuzzle" && game != "cardflip") {
+                bestScores[game] = json[i].score;
+                break;
+              } else {
+                bestScores[game] = json[i].score;
+              }
+            }
+          }
+        });
+    }
+  }
   return {
     props: {
       user: data.user,
+      bestscores: bestScores,
     },
   };
 }
