7f3cbc8e0c2303469e228970bc2afb0db0037524
Stephen Ip <stephenip@berkeley.edu>
2022-05-13 23:28:04 -0700

    fix default pfp

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -55,7 +55,7 @@ export default function handler(req, res) {
           password: hash,
           placementtest: false,
           pfpurl:
-            "../public/default.jpg",
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
         };
         let result_p = users.insertOne(user);
         result_p.then(function (result) {
