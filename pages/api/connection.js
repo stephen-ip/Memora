5069695a9fe125a7187f905fc83be9f6a0e94a83
Stephen Ip <stephenip@berkeley.edu>
2022-05-13 20:22:36 -0700

    serverless-mysql syntax fix

--- pages/api/connection.js
+++ pages/api/connection.js
@@ -1,6 +1,6 @@
 import mysql from 'serverless-mysql';
 
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = mysql({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
